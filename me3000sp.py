d51633ab28de49b209964336875a44c732bc9333
Visualys <90142811+Visualys@users.noreply.github.com>
2023-11-13 18:33:02 +0100

    Update me3000sp.py

--- me3000sp.py
+++ me3000sp.py
@@ -3,20 +3,24 @@ import machine
 from utime import sleep
 import gc
 
+MODBUS_RX = 18
+MODBUS_TX = 19
+MODBUS_DE_RE = 4
+
 led = Pin(2, Pin.OUT)
 
 # Wifi Connection :
 sta = None
-def start_wifi(ssid='Ben&Steph'):
+def start_wifi(ssid='Ben&Steph', pwd='Ben&Steph'):
     global sta
     import network
-    from time import sleep 
+    from time import sleep
     if sta is None:
         sta = network.WLAN(network.STA_IF)
         sta.active(False)
         sta.active(True)
     if not sta.isconnected():
-        sta.connect(ssid, 'Ben&Steph')
+        sta.connect(ssid, pwd)
         print('wifi connection to %s' % ssid, end='')
         timeout = 0
         while not sta.isconnected():
@@ -30,8 +34,8 @@ def start_wifi(ssid='Ben&Steph'):
         print('rssi: %sdB IP: %s' % (sta.status('rssi'),sta.ifconfig()[0]))
 start_wifi()
 
-# Send to DMX webserver
-def send_to_server(d):
+# Send to webserver
+def send_to_server(txt):
     import usocket
     s = usocket.socket()
     try:
@@ -39,7 +43,7 @@ def send_to_server(d):
     except OSError:
         return 0
     s.setblocking(False)
-    ret = s.send(bytes('GET /dmx?mes={} HTTP/1.1\r\n\r\n\r\n'.format(','.join(d)), 'utf8'))
+    ret = s.send(bytes('GET /dmx?mes=%s HTTP/1.1\r\n\r\n\r\n' % txt, 'utf8'))
     s.close()
     return ret
 
@@ -48,14 +52,11 @@ print('start in 3s...')
 sleep(3)
 
 
-# Modbus connection :
-# GPIO4 -> DE/DE
-# GPIO18 -> DI
-# GPIO19 -> RO
-ctrl_tx = Pin(4, Pin.OUT)
+# Modbus initialisation :
+ctrl_tx = Pin(MODBUS_DE_RE, Pin.OUT)
 ctrl_tx(0)
 host = UART(2)
-host.init(baudrate=115200, bits=8, parity=None, stop=1, rx=18, tx=19, timeout=1000)
+host.init(baudrate=115200, bits=8, parity=None, stop=1, rx=MODBUS_RX, tx=MODBUS_TX, timeout=1000)
 host.read()
 
 def crc16(data):
@@ -73,70 +74,58 @@ def signedinteger(data):
     n = data[0]<<8 | data[1]
     if(n & 0x8000):
         n = (n & 0x7FFF) - 32767
-    return n 
+    return n
 
 statxt = ['Wait','Charge (check)','Charge','Discharge (check)','Discharge','EPS','Fault','Fault']
-status = 0
-volt = 0
-freq = 0
-batload = 0
-batlev = 0
-powedfin = 0
-powload = 0
-powme3000 = 0
-powpv = 0
-todaypv = 0
-todayedfout = 0
-todayedfin = 0
-todayload = 0
-batcycles = 0
-    
+s = ""
+
 def read_me3000sp():
-    global status, volt, freq, batload, batlev, powedfin, powload, powme3000, powpv,todaypv,todayedfout,todayedfin,todayload,batcycles
+    global s
     a = [1, 3, 0x02, 0x00, 0, 45]
     a+=crc16(a)
     ctrl_tx(1)
     host.write(bytes(a))
     host.flush()
     ctrl_tx(0)
+    led(1)
     r = host.read()
-    i=3
-    status = statxt[r[i]<<8 | r[i+1]]
-    i+=12
-    volt = (r[i]<<8 | r[i+1]) * .1
-    i+=12
-    freq = (r[i]<<8 | r[i+1]) * .01
-    i+=2
-    batload = -signedinteger((r[i], r[i+1])) * 10
-    i+=6
-    batlev = (r[i]<<8 | r[i+1])
-    i+=4
-    powedfin = -signedinteger((r[i], r[i+1])) * 10
-    i+=2
-    powload = (r[i]<<8 | r[i+1]) * 10
-    i+=2
-    powme3000 = signedinteger((r[i], r[i+1])) * 10
-    i+=2
-    powpv = (r[i]<<8 | r[i+1]) * 10
-    i+=6
-    todaypv = (r[i]<<8 | r[i+1]) * 10
-    i+=2
-    todayedfout = (r[i]<<8 | r[i+1]) * 10
-    i+=2
-    todayedfin = (r[i]<<8 | r[i+1]) * 10
-    i+=2
-    todayload = (r[i]<<8 | r[i+1]) * 10
-    i+=34
-    batcycles = r[i]<<8 | r[i+1]
+    led(0)
+    if len(r)==95: 
+        s = ""
+        i=3
+        s='26,' + statxt[r[i]<<8 | r[i+1]] # status
+        i+=12
+        s+=',11,' + str((r[i]<<8 | r[i+1]) * .1) # voltage
+        i+=12
+        s+=',12,' + str((r[i]<<8 | r[i+1]) * .01) # frequency
+        i+=8
+        s+=',25,' + str(r[i]<<8 | r[i+1]) # batery level
+        i+=4
+        s+=',32,' + str(-signedinteger((r[i], r[i+1])) * 10) # power grid input
+        i+=2
+        s+=',33,' + str((r[i]<<8 | r[i+1]) * 10) # power home load
+        i+=2
+        s+=',27,' + str(signedinteger((r[i], r[i+1])) * 10) # power me3000
+        i+=2
+        s+=',31,' + str((r[i]<<8 | r[i+1]) * 10) # power pv
+        i+=6
+        s+=',28,' + str((r[i]<<8 | r[i+1]) * 10) # today pv
+        i+=2
+        s+=',34,' + str((r[i]<<8 | r[i+1]) * 10) # todayedfout
+        i+=2
+        s+=',29,' + str((r[i]<<8 | r[i+1]) * 10) # todayedfin
+        i+=2
+        s+=',30,' + str((r[i]<<8 | r[i+1]) * 10) # todayload
+        i+=34
+#       s+='**,' + str(r[i]<<8 | r[i+1]) # batcycles
+        return 1
+    return 0
 
 def run():
     gc.collect()
     start_wifi()
-    led(1)
-    read_me3000sp()
-    led(0)
-    dic = ['25', str(batlev),'26', status, '27', str(powme3000),'28', str(todaypv),'29', str(todayedfin),'30', str(todayload),'31', str(powpv),'32', str(powedfin),'33', str(powload), '34', str(todayedfout)]
-    send_to_server(dic)
+    if read_me3000sp():
+        send_to_server(s)
 
 def autorun():
     while True:
