fd6bdcebe0a7e58ad141305a6050bf4a709f41f7
Visualys <90142811+Visualys@users.noreply.github.com>
2023-11-15 22:38:44 +0100

    Update htu21d.py

--- htu21d.py
+++ htu21d.py
@@ -1,11 +1,6 @@
 from machine import I2C, Pin
-import time
 
-class HTU21D(object):
-    ADDRESS = 0x40
-    ISSUE_TEMP_ADDRESS = 0xE3
-    ISSUE_HU_ADDRESS = 0xE5
-
+class htu21d(object):
     def __init__(self, scl, sda):
         self.i2c = I2C(scl=Pin(scl), sda=Pin(sda), freq=100000)
 
@@ -21,35 +16,32 @@ class HTU21D(object):
             return True
         else:
             return False
-
-    def _issue_measurement(self, write_address):
+        
+    def _sendcommand(self, addr):
         self.i2c.start()
-        self.i2c.writeto_mem(int(self.ADDRESS), int(write_address), '')
+        self.i2c.writeto_mem(0x40, addr, '')
         self.i2c.stop()
-        time.sleep_ms(50)
+
+    def _measure(self, addr):
+        self._sendcommand(addr)
         data = bytearray(3)
-        self.i2c.readfrom_into(self.ADDRESS, data)
+        self.i2c.readfrom_into(0x40, data)
         if not self._crc_check(data):
             raise ValueError()
         raw = (data[0] << 8) + data[1]
         raw &= 0xFFFC
         return raw
-
     @property
     def temperature(self):
-        """Calculate temperature"""
-        raw = self._issue_measurement(self.ISSUE_TEMP_ADDRESS)
-        return -46.85 + (175.72 * raw / 65536)
-
+        raw = self._measure(0xE3)
+        return round(0.002681274414 * raw - 46.85, 2)
     @property
     def humidity(self):
-        """Calculate humidity"""
-        raw =  self._issue_measurement(self.ISSUE_HU_ADDRESS)
-        return -6 + (125.0 * raw / 65536)
-
-
-#sensor = HTU21D(22,21)
-#hum = sensor.humidity
-#temp = sensor.temperature
-#print('Humidity : ', + hum)
-#print('Temperature : ', + temp)
+        raw =  self._measure(0xE5)
+        return round(0.001907348633 * raw - 6, 2)
+    @property
+    def lowbattery(self):
+        self._sendcommand(0xE7)
+        data = bytearray(1)
+        self.i2c.readfrom_into(0x40, data)
+        return True if data[0] & 0x40 else False
